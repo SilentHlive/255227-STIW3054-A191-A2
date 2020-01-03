7a443b3cfc729cd2eb05425f405689016eae437a
SilentHLive <halimahramli6596@gmail.com>
2020-01-03 21:30:46 +0800

    Updating again....

---
+++ src/main/java/stiw3054/FollowerDetails.java
@@ -0,0 +1,124 @@
+package stiw3054;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FollowerDetails {
+    int countR = 0;
+    int countS =0;
+    int countFg =0;
+    int countFr =0;
+    int n;
+    int i = 0;
+    String id;
+    ArrayList<FollowerDetails> f = new ArrayList<FollowerDetails>( );
+
+    FollowerDetails() {
+    }
+
+
+    FollowerDetails(int n,String id, int countR, int countFr,int countS, int countFg) {
+        this.n=n;
+        this.id = id;
+        this.countR = countR;
+        this.countFr = countFr;
+        this.countS = countS;
+        this.countFg = countFg;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getCountR() {
+        return countR;
+    }
+
+    public int getCountFg() {
+        return countFg;
+    }
+
+    public int getCountFr() {
+        return countFr;
+    }
+
+    public int getCountS() {
+        return countS;
+    }
+
+    public void listFollower(int n,String id, int countR,int countFg, int countFr, int countS) throws IOException {
+        FollowerDetails ff = new FollowerDetails(n,id,countR,countFr,countS,countFg);
+        f.add(ff);
+        i++;
+    }
+
+    public void FollowerList() {
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook( );
+
+            XSSFSheet sheet = workbook.createSheet("follower details list");// creating a blank sheet
+
+            Row row = sheet.createRow(0);
+            Cell cell = row.createCell(0);
+            Cell cell1 = row.createCell(1);
+            Cell cell2 = row.createCell(2);
+            Cell cell3 = row.createCell(3);
+            Cell cell4 = row.createCell(4);
+            Cell cell5 = row.createCell(5);
+            cell.setCellValue("No");
+            cell1.setCellValue("Login ID");
+            cell2.setCellValue("Number of repositories");
+            cell3.setCellValue("Number of Following");
+            cell4.setCellValue("Number of Followers");
+            cell5.setCellValue("Number of Stars");
+            int rownum = 1;
+            for (FollowerDetails listf : f) {
+                Row row1 = sheet.createRow(rownum++);
+                createList(listf, row1);
+
+            }
+
+            FileOutputStream out = new FileOutputStream(new File("C:\\Users\\HALIMAH\\IdeaProjects\\_255227_Asg2\\excel\\Report.xlsx")); // file name with path
+            for(int j =0; j<7;j++){
+                sheet.autoSizeColumn(j);
+            }
+
+            workbook.write(out);
+            out.close( );
+
+        } catch (Exception e) {
+            e.printStackTrace( );
+        }
+    }
+
+    public static void createList(FollowerDetails f, Row row) // creating cells for each row
+    {
+
+        Cell cell = row.createCell(0);
+        Cell cell1 = row.createCell(1);
+        Cell cell2 = row.createCell(2);
+        Cell cell3 = row.createCell(3);
+        Cell cell4 = row.createCell(4);
+        Cell cell5 = row.createCell(5);
+
+        cell.setCellValue(f.getN( ));
+        cell1.setCellValue(f.getId( ));
+        cell2.setCellValue(f.getCountR( ));
+        cell3.setCellValue(f.getCountFg( ));
+        cell4.setCellValue(f.getCountFr( ));
+        cell5.setCellValue(f.getCountS( ));
+    }
+
+    }
+
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"stiw3054","l":"Follower"},{"p":"stiw3054","l":"FollowerDetails"},{"p":"stiw3054","l":"FollowerList"},{"p":"stiw3054","l":"Main"}]
